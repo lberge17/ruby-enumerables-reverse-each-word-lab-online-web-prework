684d2e03d0c6342488daf5f83bc388db3aef2f8f
Laura Berge <lauraeberge@gmail.com>
2019-08-29 23:15:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   new = sentence.split
-  puts new.collect {|word| word.reverse}.join(" ")
+  p new.collect {|word| word.reverse}.join(" ")
 end
 
 reverse_each_word("Hello there, and how are you?")
