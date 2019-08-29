c12b74e79a7edcb945f2ccbe0e5c072d2596c093
Laura Berge <lauraeberge@gmail.com>
2019-08-29 22:58:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = []
   #sentence.chars.reverse
-  sentence.split.map{|word| word.reverse}.join("")
+  sentence.split.map {|word| word.reverse}.join(" ")
 end
 
 
